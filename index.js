26b6ddce3580fc194a7ce16a54ba738b25ce6a06
Kesha <kesha.leushin@gmail.com>
2019-02-06 20:11:26 +0300

    add async await

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
 const files = [];
 
-const getFiles = (dir) => {
-    fs.readdir(dir, (err, items) => {
-        items.forEach(item => {
-            const itemPath = path.join(dir, item);
-            fs.stat(itemPath, (err, stats) => {
-                stats.isDirectory() ? getFiles(itemPath) : files.push({ item, itemPath });
-            });
-        });
+const readdir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
+
+const getFiles = async (dir, callback) => {
+    const items = await readdir(dir);
+
+    items.forEach(async item => {
+        const itemPath = path.join(dir, item);
+        const stats = await stat(itemPath);
+        stats.isDirectory() ? getFiles(itemPath) : files.push({ item, itemPath });
     });
 };
 
-getFiles('pictures');
+getFiles('pictures').then(() => {
+    console.log(files);
+});
